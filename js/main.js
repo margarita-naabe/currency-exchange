fd75c380ddebf7bf75a88008053c5b395bfab8ee
margarita-naabe <margaritanaabe.mn@gmail.com>
2021-04-25 14:42:50 +0000

    amended input fields and js file

--- js/main.js
+++ js/main.js
@@ -1,123 +1,33 @@
-jQuery(document).ready(function () {
-	// some variables
-  
-	var baseCurrency = 'USD';
-  
-	var baseNumber = 1;
-  
-	var targetCurrency = 'INR';
-  
-	var targetNumber;
-  
-	var url;
-  
-	currencyConverter(baseCurrency, baseNumber,targetCurrency,targetNumber)
-  
-	// get base currency value
-  
-	$("#base").change(function () {
-	  // base currency
-  
-	  baseCurrency = $(this).children("option:selected").val();
-  
-	  // call currencyConverter function
-  
-	  currencyConverter(baseCurrency,baseNumber,targetCurrency,targetNumber)
-  
-  
-	});
-  
-	// get base currency number
-  
-	$("#baseNumber").change(function(){
-  
-	  // base number
-  
-	  baseNumber = $(this).val()
-  
-	  // call currencyConverter function
-  
-	  currencyConverter(baseCurrency,baseNumber,targetCurrency,targetNumber)
-  
-	})
-  
-	// get target currency value
-  
-	$("#target").change(function () {
-	  // target currency
-  
-	  targetCurrency = $(this).children("option:selected").val();
-  
-	  // call currencyConverter function
-  
-	  currencyConverter(baseCurrency,baseNumber,targetCurrency,targetNumber)
-  
-	});
-  
-	// get target currency number
-  
-	$("#targetNumber").change(function(){
-  
-	  // target number
-  
-	  targetNumber = $(this).val()
-  
-	  // call currencyConverter function
-  
-	  currencyConverter2(baseCurrency,baseNumber,targetCurrency,targetNumber)
-  
-	})
-  
-  
-	// function to convert BaseCurrency to TargetCurrency
-  
-	function currencyConverter(baseCurrency, baseNumber,targetCurrency,targetNumber)
-	{
-		// api url
-  
-		url = "https://api.exchangeratesapi.io/latest?symbols="+targetCurrency+"&base="+baseCurrency
-  
-		// make a get request to api
-  
-	  
-  
-		$.get(url,function(data){
-			console.log(data.rates)
-  
-			for (let [key, value] of Object.entries(data.rates)) {
-			  
-			  var result = value * baseNumber
-  
-			 $("#targetNumber").val(result)
-  
-			}
-			console.log(`data.rates.${targetCurrency}`)
-		})
-	}
-  
-	function currencyConverter2(baseCurrency, baseNumber,targetCurrency,targetNumber)
-	{
-		// api url
-  
-		url = "https://api.exchangeratesapi.io/latest?symbols="+baseCurrency+"&base="+targetCurrency
-  
-		// make a get request to api
-  
-		$.get(url,function(data){
-			console.log(data.rates)
-  
-			for (let [key, value] of Object.entries(data.rates)) {
-  
-			  console.log(value)
-			  
-			  var result = value * targetNumber
-  
-			 $("#baseNumber").val(result)
-  
-			}
-			console.log(`data.rates.${targetCurrency}`)
-		})
-	}
-  
-  
-  });
+const from_currencyEl = document.getElementById('from_currency');
+const from_ammountEl = document.getElementById('from_ammount');
+const to_currencyEl = document.getElementById('to_currency');
+const to_ammountEl = document.getElementById('to_ammount');
+const rateEl = document.getElementById('rate');
+const exchange = document.getElementById('exchange');
+ 
+from_currencyEl.addEventListener('change', calculate);
+from_ammountEl.addEventListener('input', calculate);
+to_currencyEl.addEventListener('change', calculate);
+to_ammountEl.addEventListener('input', calculate);
+ 
+exchange.addEventListener('click', () => {
+ const temp = from_currencyEl.value;
+ from_currencyEl.value = to_currencyEl.value;
+ to_currencyEl.value = temp;
+ calculate();
+});
+ 
+function calculate() {
+ const from_currency = from_currencyEl.value;
+ const to_currency = to_currencyEl.value;
+ 
+ fetch(`https://api.exchangerate-api.com/v4/latest/${from_currency}`)
+ .then(res => res.json())
+ .then(res => {
+ const rate = res.rates[to_currency];
+ rateEl.innerText = `1 ${from_currency} = ${rate} ${to_currency}`
+ to_ammountEl.value = (from_ammountEl.value * rate).toFixed(2);
+ })
+}
+ 
+calculate();
